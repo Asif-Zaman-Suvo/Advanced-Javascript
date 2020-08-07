07bfe5804c907f3bbdf4945f9356d38dd681d7a1
ShuvoCSE176 <asifshuvo119181@gmail.com>
2020-08-07 18:22:58 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+let pakhi;
+
+
+function add(num1,num2){
+    console.log(num1+num2);
+    return
+}
+
+
+function add (num1 , num2){
+    console.log(num1,num2);
+}
+
+
+const premik={name:"smart dude",phone:34567}
+
+
+console.log(premik.gf);
+
+
+let result=undefined;
+console.log(result);
+
+let ages=[3,5,6,7];
+console.log(ages[12]);
